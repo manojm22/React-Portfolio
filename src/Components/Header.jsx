3d62ce558456e4880988c3dc5055bfd8e163172d
manojm22 <manojmenon222@gmail.com>
2025-06-09 15:13:08 +0530

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <div>logo</div>
+      <div>header list</div>
+      <div>links</div>
+      <div>button</div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Components/sample.jsx
@@ -0,0 +1,20 @@
+import React, { Component, useState } from 'react'
+
+export class Sample extends Component {
+  render() {
+    return (
+      <div>
+        hello        
+      </div>
+    )
+  }
+}
+
+export function Asdf() {
+  return (
+    <div>
+      asdf
+    </div>
+  )
+}
+
