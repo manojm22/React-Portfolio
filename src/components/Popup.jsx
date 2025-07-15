cda39c79c289cab030f913ee2390af49daa968bf
manojm22 <manojmenon222@gmail.com>
2025-07-15 15:26:14 +0530

    sdvsfdg

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -27,23 +27,23 @@ const Popup = ({ testimonial, onClose }) => {
           <div className=" h-56 rounded-t-xl bg-gradient-to-r from-green-500 to-blue-500 p-4 flex items-center justify-center">
             <img
               src={testimonial.image}
-              alt={testimonial.name}
+              alt={testimonial.Name}
               className="w-20 h-20 rounded-full"
             />
           </div>
           <motion.div className="flex flex-col justify-center items-center p-4 gap-4">
             <h3 className="text-base sm:text-lg md:text-xl font-semibold">
-              {testimonial.name}
+              {testimonial.Name}
             </h3>
             <motion.div className="text-xs sm:text-sm md:text-base text-gray-500 ">
-              {testimonial.designation} at {testimonial.company}
+              {testimonial.Designation} at {testimonial.Company}
             </motion.div>
             <motion.p //direction, type, delay, duration
               className="mt-4 text-secondary 
                   text-sm sm:text-base md:text-m max-w-3xl 
                   leading-[26px] sm:leading-[30px] "
             >
-              {testimonial.testimonial}
+              {testimonial.Comment}
             </motion.p>
           </motion.div>
         </div>
